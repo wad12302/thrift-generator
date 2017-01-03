8e45c14193443771fa209be726d014390ade4235
weisd <weisd@eptok.com>
2017-01-03 18:19:49 +0800

    如果接口参数涉及到继承问题public class AccDo extends User

---
+++ src/test/java/com/sohu/thrift/generator/test/AccDo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sohu.thrift.generator.test;
+
+public class AccDo extends User {
+
+	private String testExtends;
+
+	public String getTestExtends() {
+
+		return testExtends;
+	}
+
+	public void setTestExtends(String testExtends) {
+		this.testExtends = testExtends;
+	}
+
+}
